bbd4df3397285788e46a4f073c05b4b44ea6540d
jichenjc <jichenjc@cn.ibm.com>
2017-12-04 14:37:32 +0800

    Add init support

---
+++ zvmsdk/provider.go
@@ -0,0 +1,28 @@
+package zvmsdk
+
+import (
+	"github.com/hashicorp/terraform/helper/mutexkv"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// Global poolMutexKV
+var poolMutexKV = mutexkv.NewMutexKV()
+
+// Provider libvirt
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"uri": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LIBVIRT_DEFAULT_URI", nil),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"zvm_guest":    resourceZVMGuest(),
+		},
+
+	}
+}
