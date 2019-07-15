8d04ed95c1924e12c95039c2e0e2bfc87d46898b
ji chen <jichenjc@cn.ibm.com>
2019-07-15 10:20:43 +0000

    Fix folder
    Change-Id: I96a317604321fa96022a962a1b012ef411262901

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/terraform-provider-zvmsdk/zvmsdk"
+	"github.com/mfcloud/terraform-provider-zvmsdk/zvmsdk"
 	"github.com/hashicorp/terraform/plugin"
 	"math/rand"
 	"time"
